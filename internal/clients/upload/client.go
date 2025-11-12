43041556e7baefe3bfa88705e4c2c314b03d4853
anca cismasiu <anca.cismasiu@snyk.io>
2025-11-12 21:14:30 +0200

    feat: call new file upload

--- internal/clients/upload/client.go
+++ internal/clients/upload/client.go
@@ -1,35 +1,33 @@
 package upload
 
 import (
-	"context"
 	"fmt"
-
-	"github.com/google/uuid"
 
 	"github.com/snyk/go-application-framework/pkg/apiclients/fileupload"
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/snyk/go-application-framework/pkg/workflow"
 )
 
-type Client interface {
-	CreateRevisionFromChan(ctx context.Context, paths <-chan string, baseDir string) (fileupload.UploadResult, error)
-}
-
-type FileUploadClient struct {
+type Client struct {
 	fileupload.Client
+	// currently using https://github.com/snyk/go-application-framework/pull/460/files
 }
 
-func NewClient(ictx workflow.InvocationContext, orgID string) (*FileUploadClient, error) {
-	org, err := uuid.Parse(orgID)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse orgID: %s: %w", orgID, err)
-	}
 
+func NewClient(ictx workflow.InvocationContext) (*Client, error) {
 	config := ictx.GetConfiguration()
 	httpClient := ictx.GetNetworkAccess().GetHttpClient()
 	baseURL := config.GetString(configuration.API_URL)
-	cfg := fileupload.Config{BaseURL: baseURL, OrgID: org}
+	fuConfig := fileupload.Config{BaseURL: baseURL}
+	
+	uploadClient := fileupload.NewClient(httpClient, fuConfig)
+	
+	// TODO proper error handling?
+	if uploadClient == nil {
+		return nil, fmt.Errorf("failed to create test API client: %w", nil)
+	}
 
-	uploadClient := fileupload.NewClient(httpClient, cfg, fileupload.WithLogger(ictx.GetEnhancedLogger()))
-	return &FileUploadClient{uploadClient}, nil
+	return &Client{
+		uploadClient,
+	}, nil
 }
